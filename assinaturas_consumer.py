251e4b3331d2552d1ec2146071ee6ceea42c4280
Igor <igorcostalins2005@gmail.com>
2025-01-22 17:18:13 -0300

    Reestruturação do celery e do consumer

--- assinaturas_consumer.py
+++ assinaturas_consumer.py
@@ -1,27 +1,19 @@
-import pandas as pd
-import numpy as np
 from tasks import calcularGaussianNB
+import pandas as pd 
 from celery import group
 
-diabetes_binario = pd.read_csv('dados/data_tratado.csv')
-matriz_possibilidades = pd.read_csv('dados/matriz_possibilidades.csv')
-
-
-total_linhas = 2**21 - 1
+total_linhas = 100000
 lista_assinaturas = []
 
-for i in range(1000):
-    array_posicao = np.array(matriz_possibilidades.iloc[i])
-    diabetes_posicao = []
-    
-    for k in range(21):
-        if array_posicao[k] == 1:
-            diabetes_posicao.append(diabetes_binario[f'{k}'])
-    
-    print(f"Processando linha {i}")
-    lista_assinaturas.append(calcularGaussianNB.s(diabetes_posicao))
+df_possibilidades = pd.read_csv('dados/matriz_possibilidades.csv')
+
+for linha in range(total_linhas):
+    print(linha)
+    possibilidade = (df_possibilidades.iloc[linha]).tolist()
+    lista_assinaturas.append(calcularGaussianNB.s(possibilidade))
 
 grupo_tarefas = group(lista_assinaturas)
-resultado = grupo_tarefas.apply_async()
+resultados = grupo_tarefas.apply_async()
 
-print(resultado.get)
+for cont, resultado in enumerate(resultados):
+    print(f'Resultado da task {cont}: ', resultado.get())
--- tasks.py
+++ tasks.py
@@ -3,22 +3,37 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import recall_score
 from celery import Celery
 import pandas as pd
+import numpy as np
+from pydantic import BaseModel
+from typing import List
+
+class ModeloLista(BaseModel):
+    combinacao: List[int]
 
 app = Celery('tasks', broker='amqp://guest:guest@localhost//', backend="redis://")
 
 priors = [218334 / 253680, 35346 / 253680]
 model = GaussianNB(priors=priors)
 
-y = pd.read_csv('dados/y_diabetes.csv', header=None)
+diabetes_binario = pd.read_csv('dados/data_tratado.csv')
+diabetes_padrao = pd.read_csv('dados/diabetes_binario.csv')
+y = diabetes_padrao.pop('Diabetes_binary')
 
 @app.task
-def calcularGaussianNB(data):
-    data = pd.DataFrame(data).transpose()
-    x_treino, x_teste, y_treino, y_teste = train_test_split(data, y, test_size=0.2, random_state=42)
+def calcularGaussianNB(vetor: list[int]):
+
+    diabetes_combinacao = []
+    for posicao, elemento in enumerate(vetor):
+        if elemento == 1:
+            diabetes_combinacao.append(diabetes_binario[f'{posicao}'])
+
+    data = pd.DataFrame(diabetes_combinacao).transpose()
+    x_treino, x_teste, y_treino, y_teste = train_test_split(data, y, test_size=0.2)
 
     model.fit(x_treino, y_treino)
     y_pred = model.predict(x_teste)
     recall = recall_score(y_teste, y_pred, average=None)
-    print('Recall: ', recall, '\n')
-    return recall
+    recall_util = float(recall[1])
+
+    return recall_util
 
